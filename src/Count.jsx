a409ea63f8047a6ef8f597f03f992b4d0406e925
waxir <danishdarman06@gmail.com>
2026-03-04 23:48:46 +0500

    increase,decrease,reset is commited

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 
-function Count() {
-  
-    const [count, setCount] = useState(0)
+function Counter() {
+  const [count, setCount] = useState(0);
 
-    let incCount = ()=>{
-        setCount(count +1)
-    }
-    
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
 
   return (
-    <div>
-      <h3>Count = {count}</h3>
-      <button onClick={incCount}>increase count</button>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
+      <button onClick={reset} style={{ marginLeft: "10px" }}>
+        Reset
+      </button>
     </div>
   );
 }
 
-export default Count;
+export default Counter;
